8de459a8ccf1b51f5be779b9fe8bc02b81b61d5e
Sandro Janotte <sjanotte@gmail.com>
2020-11-21 18:54:35 -0300

    Developed delete person by id

--- src/main/java/dio/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/dio/digitalinnovation/personapi/service/PersonService.java
@@ -1,7 +1,6 @@
 package dio.digitalinnovation.personapi.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,13 +39,15 @@ public class PersonService {
 	}
 
 	public PersonDTO findById(Long id) throws PersonNotFoundException {
-		Optional<Person> optionalPerson = personRepository.findById(id);
-		
-		if (optionalPerson.isEmpty()) {
-			throw new PersonNotFoundException(id);
-		}
-		
-		return personMapper.toDTO(optionalPerson.get());
+		Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+
+		return personMapper.toDTO(person);
+	}
+
+	public void delete(Long id) throws PersonNotFoundException {
+		personRepository.findById(id)
+			.orElseThrow(() -> new PersonNotFoundException(id));
+		personRepository.deleteById(id);
 	}
 
 }
